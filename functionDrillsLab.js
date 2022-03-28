7f66b1ca73d7dfbba7a6b9f6678175b944ce7225
Spencer DeBrine <debst12@gmail.com>
2022-03-28 16:06:36 -0600

    Problems 1-16 Solutions

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -252,6 +252,14 @@ let bigOrSmallArray = [1,101, 102, 2, 103, 4, 5, 6, 107]
 */
 
 //CODE HERE
+const bigOrSmall = arr => {
+  let answers = []
+  for (let i = 0; i < bigOrSmallArray.length; i++) {
+    arr[i] > 100 ? answers.push('big') : answers.push('small')
+}
+console.log(answers)
+}
+bigOrSmall(bigOrSmallArray)
 
 
 ////////////////// PROBLEM 16 ////////////////////
