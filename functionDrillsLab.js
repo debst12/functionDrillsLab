d4fdd7cc009d764628180478bd831f51f2d43549
Spencer DeBrine <debst12@gmail.com>
2022-03-28 17:13:57 -0600

    Problems 1-22 Solutions - Final Commit

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -71,13 +71,8 @@ console.log(greeting('Spencer'))
 */
 
 //CODE HERE
-const compareNums = (num1,num2) => {
-  if(num1 === num2) {
-    return num1
-  } else {
-    Math.max(num1,num2)
-  }
-}
+const compareNums = (num1,num2) => num1 >= num2 ? num1 : num2
+
 console.log(compareNums(1,2))
 ////////////////// PROBLEM 6 ////////////////////
 /*
@@ -373,13 +368,13 @@ function pond() {
 */
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = []
+let globalScope = ['duck']
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = []
+let bathroomScope = ['rubberDuck']
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = []
+let bathtubScope = ['sailorDuck']
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = []
+let pondScope = ['realDuck']
